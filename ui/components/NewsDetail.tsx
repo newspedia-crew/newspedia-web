0a97a46957850894552d14587ffc6f5baed19728
Yifei Hu <normalmoc@icloud.com>
2024-07-19 16:50:03 +0800

    Change the position to right most

--- ui/components/NewsDetail.tsx
+++ ui/components/NewsDetail.tsx
@@ -42,9 +42,9 @@ interface NewsDetailProperties {
 
 const NewsDetail: React.FC<NewsDetailProperties> = ({ news }) => {
   return (
-    <div className="relative max-w-6xl mx-auto px-4 lg:px-0">
-      <div className="lg:flex lg:space-x-8">
-        <article className="prose lg:prose-xl dark:prose-invert lg:w-3/4">
+    <div className="relative max-w-full mx-auto">
+      <div className="flex justify-center">
+        <article className="prose lg:prose-xl dark:prose-invert max-w-4xl w-full px-4 lg:px-0">
           <h1 className="text-black dark:text-white">{news.title}</h1>
           {news.sections.map((section, index) => (
             <Element name={`section-${index}`} key={index}>
@@ -59,7 +59,7 @@ const NewsDetail: React.FC<NewsDetailProperties> = ({ news }) => {
             </Element>
           ))}
         </article>
-        <div className="lg:w-1/4">
+        <div className="hidden xl:block fixed right-0 top-0 w-64 h-full overflow-auto">
           <NewsSidebar sections={news.sections} />
         </div>
       </div>
